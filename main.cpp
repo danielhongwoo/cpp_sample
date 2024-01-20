ad70badac34811250bc7f06d98e97d7727d8f10a
Daniel Hongwoo Lee <daniel.hongwoo.lee@gmail.com>
2024-01-20 08:43:54 +0000

    task: add docker environment

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 int main(int, char **) {
-  std::cout << add(1, 2) << std::endl;
+  std::cout << "add(1, 2): " << add(1, 2) << std::endl;
+  std::cout << "add(3, 4): " << add(3, 4) << std::endl;
+  
   return 0;
 }
